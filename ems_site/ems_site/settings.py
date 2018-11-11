71e9bc14122655269b7f9abf68b54b3d2e96676b
Brian Tang <byron123t@gmail.com>
2018-11-11 00:23:05 -0600

    Add settings

--- ems_site/ems_site/settings.py
+++ ems_site/ems_site/settings.py
@@ -118,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_media/')
+
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "emt/static"),
+        os.path.join(BASE_DIR, "hosp/static")
+        ]
