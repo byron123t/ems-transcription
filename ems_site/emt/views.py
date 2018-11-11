43b4cb427534969baa3513d4156d0d8c7b8a09cc
TeaOfJay <TeaOfJay@users.noreply.github.com>
2018-11-10 23:03:39 -0600

    changes to apps and general changes to views and html

--- ems_site/emt/views.py
+++ ems_site/emt/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+
+def get_hospital(request):
+
+	hospitals = {} #populate based on api
+
+	return render(request, 'emt/hospital_form.html', {'hospitals': hospitals})
+
+
+
+def get_triage(request):
+
+
+	return render(request, 'emt/triage_form.html')
--- ems_site/healthcare_provider/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HealthcareProviderConfig(AppConfig):
-    name = 'healthcare_provider'
